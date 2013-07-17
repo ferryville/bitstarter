97789b4c68fa8f0956e01047168a3aea2184c726
ferryville <aymenbensaid@gmail.com>
2013-07-17 22:40:54 +0000

    Revert "Update index html"
    This reverts commit 01cf2a70f5aa44c972ab87fef9d2a2a2e51908ae.

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require('express');
-var fs = require('fs');
-var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {  
-var html =  fs.readFileSync(htmlfile).toString();
-response.send(html);
+var fs= require('fs').readFileSync("./index.html").toString('utf-8');
+response.send(fs);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
